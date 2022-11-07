c65bcc54b98354dad0c0aec3186111a5a9a0e5a1
Mehedi <94160349+mdnuruzzamanty4@users.noreply.github.com>
2022-11-07 23:17:28 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -188,7 +188,16 @@ async def upload(m, bot, chat_id, dir):
     await m.unpin()
     await bot.send_message(
         chat_id=chat_id,
-        text=f"Succesfully Uploaded {up} Files to Telegram.\nIf you found me helpfull Join My Updates Channel"
+        text=f"Succesfully Uploaded {up} Files to Telegram.\nIf you found me helpfull Join My Updates Channel",
+        reply_markup=InlineKeyboardMarkup(
+            [
+				[
+					InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
+                    InlineKeyboardButton("⚡️Update Channel", url="https://t.me/subin_works")
+				]
+			]
+			)
+		)
     total=TOTAL
     up=0
     rm=TOTAL
